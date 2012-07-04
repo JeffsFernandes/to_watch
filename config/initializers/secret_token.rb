680fd4f47ea48055ff01663356569c7c024a07f3
Jefferson Fernandes <jeffs.fernandes@gmail.com>
2012-07-03 23:36:43 -0300

    Creating to_watch project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ToWatch::Application.config.secret_token = '8393a522b42b614ab903180f03f840fc0c9f3cf45f14f6b65bb253262696d58ae88f284e814f29e9ee16aa083b5e103d852cfe32f971dd85494b1b68c8d6e4e6'
